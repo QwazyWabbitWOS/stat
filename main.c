24b461ce600019452509e0839173968f3dbf3959
QwazyWabbit <gandalf.2004@gmail.com>
2022-03-23 12:57:17 -0700

    Cleanup and simplify.

--- main.c
+++ main.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <sys/stat.h>
+#include <sys/types.h>
 
-#ifdef _WIN32
+// If we're doing MSVC we rename the POSIX functions.
+#if defined (_WIN32) && defined (_MSC_VER)
 #include <Windows.h>
 #include <direct.h>
+#define getcwd _getcwd
 #else
 #include <unistd.h>
-#define MAX_PATH PATH_MAX
 #endif
 
+// Because MSVC doesn't define these.
 #ifndef S_ISDIR
 #define S_ISDIR(mode)  (((mode) & S_IFMT) == S_IFDIR)
 #endif
@@ -17,28 +20,24 @@
 #define S_ISREG(mode)  (((mode) & S_IFMT) == S_IFREG)
 #endif
 
-
-int main(int argc, char* argv[])
+void ShowMacros(void)
 {
-    const char* folder;
-    folder = "./tmp";
-    struct stat sb;
-    char buf[300];
-
 #ifdef _CRT_NONSTDC_NO_WARNINGS
-    printf("_CRT_NONSTDC_NO_WARNINGS is %i\n", _CRT_NONSTDC_NO_WARNINGS);
+	printf("_CRT_NONSTDC_NO_WARNINGS is %i\n", _CRT_NONSTDC_NO_WARNINGS);
 #endif
 
 #ifdef _CRT_DECLARE_NONSTDC_NAMES
-    printf("_CRT_DECLARE_NONSTD_NAMES is %i\n", _CRT_DECLARE_NONSTD_NAMES);
+	printf("_CRT_DECLARE_NONSTD_NAMES is %i\n", _CRT_DECLARE_NONSTD_NAMES);
 #endif
+}
 
-    printf("Current directory is %s\n", getcwd(buf, sizeof buf-1));
-    printf("Did we find %s there?\n", folder);
+int main(int argc, char* argv[])
+{
+	const char* folder;
+	folder = "./tmp";
+	struct stat sb;
+	char buf[1000];
 
-    if (stat(folder, &sb) == 0 && S_ISDIR(sb.st_mode)) {
-        printf("YES\n");
-    } else {
-        printf("NO\n");
-    }
+	printf("Current directory is %s\n", getcwd(buf, sizeof buf - 1));
+	printf("Did we find %s there? %s\n", folder, (stat(folder, &sb) == 0 && S_ISDIR(sb.st_mode)) ? "YES" : "NO");
 }
